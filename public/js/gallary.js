927e0fa5bf72eb5c4ea4aee6158a33311a1cc34d
crandonbruz <bcruz12127@gmail.com>
2023-08-14 14:17:31 -0400

    added stadium gallary

--- public/js/gallary.js
+++ public/js/gallary.js
@@ -1,20 +1,32 @@
-function start() {
-    var rotator = document.getElementById('rotator');  
-    var imageDir = 'C:\\Users\\Shady\\Downloads\\restaurant\\';                          
-    var delayInSeconds = 1;                            
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    var images = ['food1.jpg', 'food2.jpg', 'food3.jpg', 'food4.jpg', 'food5.jpg', 'food6.jpg', 'food7.jpg', 'food8.jpg'];
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-    var num = 0;
-    var changeImage = function() {
-        var len = images.length;
-        rotator.src = imageDir + images[num++];
-        if (num == len) {
-            num = 0;
-        }
-    };
-    setInterval(changeImage, delayInSeconds * 1000);
-};
-window.onload=function(){
-start();
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
