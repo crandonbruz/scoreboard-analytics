ece67c30a96232fb2e2782e5cf66f60223c95277
Justin Moore <justinmoore@Justins-MacBook-Air.local>
2023-08-14 13:04:55 -0400

    fixed current date, added all positions

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-fetch("https://api.sportsdata.io/v3/mlb/projections/json/BakerPlayerGameProjections/2023-08-12?key=4bcea2e7b0a647bba0a34522a4ee04b1").then(data => {
+// const dayjs = require("dayjs");
+// const yesterday = dayjs().subtract(1, 'day');
+// const currentDate = `$("#currentDay").text(dayjs().format("dddd, MM DD"))`;
+// const newDate = `${yesterday.format("YYYY")}-${yesterday.format("MMM").toUpperCase()}-${yesterday.format("DD")}`
+const currentDate = new Date().toISOString().slice(0, 10); // Get current date in 'YYYY-MM-DD' format
+
+
+
+fetch(`https://api.sportsdata.io/v3/mlb/projections/json/PlayerGameProjectionStatsByDate/${currentDate}?key=1694be448de4478ba2c3201aeb2ad64d`).then(data => {
     //as long as date in the fetch changes each day, the arrays will update themselves 
     return data.json()
 }).then(data => {
@@ -10,6 +18,9 @@ fetch("https://api.sportsdata.io/v3/mlb/projections/json/BakerPlayerGameProjecti
             Name: data[i].Name,
             Team: data[i].Team,
             Position: data[i].Position,
+            Opponent: data[i].Opponent,
+            FantasyPoints: data[i].FantasyPoints
+
             // Add More things to the card
         }
         if (data[i].Started === 1) {
@@ -19,6 +30,16 @@ fetch("https://api.sportsdata.io/v3/mlb/projections/json/BakerPlayerGameProjecti
     console.log(activePlayers.length)
     const cf = []
     const rf = []
+    const lf = []
+    const ss = []
+    const firstBase = []
+    const secondBase = []
+    const thirdBase = []
+    const catcher = []
+    const startingPitcher = []
+
+
+    // const
     // make const for every position
     for (let i = 0; i < activePlayers.length; i++) {
 
@@ -28,19 +49,139 @@ fetch("https://api.sportsdata.io/v3/mlb/projections/json/BakerPlayerGameProjecti
         if (activePlayers[i].Position === 'RF') {
             rf.push(activePlayers[i])
         }
+        if (activePlayers[i].Position === 'LF') {
+            lf.push(activePlayers[i])
+        }
+        if (activePlayers[i].Position === 'SS') {
+            ss.push(activePlayers[i])
+        }
+        if (activePlayers[i].Position === '1B') {
+            firstBase.push(activePlayers[i])
+        }
+        if (activePlayers[i].Position === '2B') {
+            secondBase.push(activePlayers[i])
+        }
+        if (activePlayers[i].Position === '3B') {
+            thirdBase.push(activePlayers[i])
+        }
+        if (activePlayers[i].Position === 'C') {
+            catcher.push(activePlayers[i])
+        }
+        if (activePlayers[i].Position === 'SP') {
+            startingPitcher.push(activePlayers[i])
+        }
         // make same if statement for every position
 
     }
     console.log(cf)
     console.log(rf)
+    console.log(lf)
+    console.log(ss)
+    console.log(firstBase)
+    console.log(secondBase)
+    console.log(thirdBase)
+    console.log(catcher)
+    console.log(startingPitcher)
+
+
     for (let player of cf) {
         const cfElement = document.querySelector(".cf")
         cfElement.innerHTML += `
 <div>
     <p> ${player.Name} </p>
-    <p> ${player.Team} </p>
+    <p>Current Team: ${player.Team} </p>
+    <p>Position: ${player.Position}</p>
+    <p>Opponent: ${player.Opponent}</p>
+    <p>Fantasy Points: ${player.FantasyPoints}</p>
 </div>`
-        // create more data for buttons
+    }
+    for (let player of rf) {
+        const rfElement = document.querySelector(".rf")
+        rfElement.innerHTML += `
+<div>
+    <p> ${player.Name} </p>
+    <p>Current Team: ${player.Team} </p>
+    <p>Position: ${player.Position}</p>
+    <p>Opponent: ${player.Opponent}</p>
+    <p>Fantasy Points: ${player.FantasyPoints}</p>
+</div>`
+    }
+    for (let player of lf) {
+        const lfElement = document.querySelector(".lf")
+        lfElement.innerHTML += `
+<div>
+    <p> ${player.Name} </p>
+    <p>Current Team: ${player.Team} </p>
+    <p>Position: ${player.Position}</p>
+    <p>Opponent: ${player.Opponent}</p>
+    <p>Fantasy Points: ${player.FantasyPoints}</p>
+</div>`
+    }
+    for (let player of ss) {
+        const ssElement = document.querySelector(".ss")
+        ssElement.innerHTML += `
+<div>
+    <p> ${player.Name} </p>
+    <p>Current Team: ${player.Team} </p>
+    <p>Position: ${player.Position}</p>
+    <p>Opponent: ${player.Opponent}</p>
+    <p>Fantasy Points: ${player.FantasyPoints}</p>
+</div>`
+    }
+    for (let player of firstBase) {
+        const firstBaseElement = document.querySelector(".firstBase")
+        firstBaseElement.innerHTML += `
+<div>
+    <p> ${player.Name} </p>
+    <p>Current Team: ${player.Team} </p>
+    <p>Position: ${player.Position}</p>
+    <p>Opponent: ${player.Opponent}</p>
+    <p>Fantasy Points: ${player.FantasyPoints}</p>
+</div>`
+    }
+    for (let player of secondBase) {
+        const secondBaseElement = document.querySelector(".secondBase")
+        secondBaseElement.innerHTML += `
+<div>
+    <p> ${player.Name} </p>
+    <p>Current Team: ${player.Team} </p>
+    <p>Position: ${player.Position}</p>
+    <p>Opponent: ${player.Opponent}</p>
+    <p>Fantasy Points: ${player.FantasyPoints}</p>
+</div>`
+    }
+    for (let player of thirdBase) {
+        const thirdBaseElement = document.querySelector(".thirdBase")
+        thirdBaseElement.innerHTML += `
+<div>
+    <p> ${player.Name} </p>
+    <p>Current Team: ${player.Team} </p>
+    <p>Position: ${player.Position}</p>
+    <p>Opponent: ${player.Opponent}</p>
+    <p>Fantasy Points: ${player.FantasyPoints}</p>
+</div>`
+    }
+    for (let player of catcher) {
+        const catcherElement = document.querySelector(".catcher")
+        catcherElement.innerHTML += `
+<div>
+    <p> ${player.Name} </p>
+    <p>Current Team: ${player.Team} </p>
+    <p>Position: ${player.Position}</p>
+    <p>Opponent: ${player.Opponent}</p>
+    <p>Fantasy Points: ${player.FantasyPoints}</p>
+</div>`
+    }
+    for (let player of startingPitcher) {
+        const startingPitcherElement = document.querySelector(".startingPitcher")
+        startingPitcherElement.innerHTML += `
+<div>
+    <p> ${player.Name} </p>
+    <p>Current Team: ${player.Team} </p>
+    <p>Position: ${player.Position}</p>
+    <p>Opponent: ${player.Opponent}</p>
+    <p>Fantasy Points: ${player.FantasyPoints}</p>
+</div>`
     }
 }).catch(err => {
     console.log(err)
