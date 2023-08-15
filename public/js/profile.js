ef77fe7f226e6fb120ed677630dfa5e39c3d5683
Cristofferb7 <136852733+Cristofferb7@users.noreply.github.com>
2023-08-15 12:02:55 -0400

    added functionality to dropdowns

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,17 +1,30 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const playerName = document.querySelector('#playerName').value.trim();
+  
   const teamName  = document.querySelector('#teamName').value.trim();
   const pitcherName  = document.querySelector('#pitcherName').value.trim();
-  const winLose  = document.querySelector('#teamName').value.trim();
+  
+  const firstBase  = document.querySelector('#firstBase').value.trim();
+  const secondBase  = document.querySelector('#secondBase').value.trim();
+  
+  const thirdBase  = document.querySelector('#thirdBase').value.trim();
+  const shortStop  = document.querySelector('#shortStop').value.trim();
+  
+  const catcher  = document.querySelector('#catcher').value.trim();
+  const leftField  = document.querySelector('#leftField').value.trim();
+  
+  const centerField  = document.querySelector('#centerField').value.trim();
+  const rightField  = document.querySelector('#rightField').value.trim();
+  const dh  = document.querySelector('#dh').value.trim();
+  
   
 
 
-  if (playerName && teamName && pitcherName && winLose) {
+  if (  teamName && pitcherName && firstBase && secondBase && thirdBase && shortStop && catcher && leftField && centerField && rightField && dh) {
     const response = await fetch(`/api/posts`, {
       method: 'POST',
-      body: JSON.stringify({ playerName, teamName  }),
+      body: JSON.stringify({  teamName, pitcherName, firstBase, secondBase, thirdBase, shortStop, catcher, leftField, centerField, rightField, dh  }),
       headers: {
         'teamName -Type': 'application/json',
       },
@@ -45,6 +58,6 @@ document
   .querySelector('.new-post-form')
   .addEventListener('submit', newFormHandler);
 
-// document
-  // .querySelector('.post-list')
-  // .addEventListener('click', delButtonHandler);
+document
+  .querySelector('.post-list')
+  .addEventListener('click', delButtonHandler);
